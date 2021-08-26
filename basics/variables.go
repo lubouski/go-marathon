a4cd567ac73176a6c4d6fcba573189f3568a397e
Aliaksandr Lubouski <aliaksandr_lubouski@epam.com>
2021-08-26 17:54:52 +0200

    golang: basics varables.go added with zero values for types examples

---
+++ basics/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	var pointer *string
+	fmt.Printf("Zero values for types: int:%v, float64:%v, bool:%v, string:%q, pointer:%v\n", i, f, b, s, pointer)
+}
+
