e4719dd122278d79e8e78e25f6b864d47ce59b05
Aliaksandr Lubouski <aliaksandr_lubouski@epam.com>
2022-03-13 11:37:43 +0100

    golang: leetcode roman to decimal converter, fix

--- leetcode/roman.go
+++ leetcode/roman.go
@@ -5,20 +5,14 @@ import (
 )
 
 func main() {
-	w := "XIV"
-
+	romanSymb := map[string]int{"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
 	var sum int
-
-	for k,_ := range w {
-		if k == len(w) - 2 && str(v) == "I" && (w[k+1] == "V" || w[k+1] == "X") {
-			if w[k+1] == "V" {
-				sum += 4
-				break
-			} else {
-				sum += 9
-				break
-			}
+	for k,v := range s {
+		if k < len(s) - 1 && romanSymb[string(v)] < romanSymb[string(s[k+1])] {
+			sum -= romanSymb[string(v)]
+		} else {
+			sum += romanSymb[string(v)]
 		}
 	}
-	fmt.Println("len", len(w))
+	fmt.Println(sum)
 }
