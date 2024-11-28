d5e634c0e5d2d6b7c9c103bc7437d0f682adf849
Alex Lubouski <lubouski@gmail.com>
2024-11-28 21:13:00 +0100

    linkedList: Merge Two Sorted Lists

---
+++ leetcode/linkedList/lc21.MergeTwoSortedLists.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	merged := &ListNode{}
+	current := merged
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
+	if list1 == nil {
+		current.Next = list2
+	} else {
+		current.Next = list1
+	}
+
+	return merged.Next
+}
+
+func main() {
+	ll1 := &ListNode{Val: 100}
+	ll2 := &ListNode{Val: 200}
+
+	fmt.Println(mergeTwoLists(ll1, ll2))
+}
