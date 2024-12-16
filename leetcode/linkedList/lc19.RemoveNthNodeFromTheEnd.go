c88eac8c26780d847accfe3f35aba9a18e9405fb
Alex Lubouski <lubouski@gmail.com>
2024-12-16 21:06:23 +0100

    linkedList: Remove Nth Node From End of List, fix

--- leetcode/linkedList/lc19.RemoveNthNodeFromTheEnd.go
+++ leetcode/linkedList/lc19.RemoveNthNodeFromTheEnd.go
@@ -4,22 +4,27 @@ import (
 	"fmt"
 )
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-    dummy := &ListNode{Next: head}
-    slowPointer := dummy
-    fastPointer := dummy
-    for i := 0; i <= n; i++ {
-        fastPointer = fastPointer.Next
-    }
-    for fastPointer != nil {
-        slowPointer = slowPointer.Next
-        fastPointer = fastPointer.Next
-    }
-    slowPointer.Next = slowPointer.Next.Next
-    return dummy.Next
+	dummy := &ListNode{Next: head}
+	slowPointer := dummy
+	fastPointer := dummy
+	for i := 0; i <= n; i++ {
+		fastPointer = fastPointer.Next
+	}
+	for fastPointer != nil {
+		slowPointer = slowPointer.Next
+		fastPointer = fastPointer.Next
+	}
+	slowPointer.Next = slowPointer.Next.Next
+	return dummy.Next
 }
 
 func main() {
 	node := &ListNode{val: 100}
-	fmt.Println(removeNthFromEnd(node, 1))	
+	fmt.Println(removeNthFromEnd(node, 1))
 }
